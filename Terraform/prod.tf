7422f06c7f647653db5606729adf917f95acdff9
kingofpirates <thousandsunny@Thousandsunny.local>
2022-05-14 22:22:22 -0400

    Save progress

---
+++ Terraform/prod.tf
@@ -0,0 +1,150 @@
+variable "whitelist" {
+  type = list(string)
+}
+variable "web_image_id" {
+  type = string
+}
+variable "web_instance_type" {
+  type = string
+}
+variable "web_desired_capacity" {
+  type = number
+}
+variable "web_max_size" {
+  type = number
+}
+variable "web_min_size" {
+  type = number
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "prod_tf_course" {
+  bucket = "tf-course-test-3"
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = "tf-course-test-3"
+  acl    = "private"
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-1b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow http and https inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.whitelist
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.whitelist
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.whitelist
+  }
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+# resource "aws_instance" "prod_web" {
+#   count = 2
+
+#   ami           = "ami-065fb54436c0e2d57"
+#   instance_type = "t2.micro"
+
+#   vpc_security_group_ids = [
+#     aws_security_group.prod_web.id
+#   ]
+#   tags = {
+#     "Terraform" : "true"
+#   }
+# }
+
+# resource "aws_eip_association" "prod_web" {
+#   instance_id   = aws_instance.prod_web.0.id
+#   allocation_id = aws_eip.prod_web.id
+# }
+
+# resource "aws_eip" "prod_web" {
+#   # instance = aws_instance.prod_web.id
+
+#   tags = {
+#     "Terraform" : "true"
+#   }
+# }
+resource "aws_elb" "prod_web" {
+  name = "prod-web"
+  # instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  tags = {
+    "Terraform" = "true"
+  }
+
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = var.web_image_id
+  instance_type = var.web_instance_type
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones = ["us-east-1", "us-east-1b"]
+  # vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity = var.web_desired_capacity
+  max_size         = var.web_max_size
+  min_size         = var.web_min_size
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
+}
